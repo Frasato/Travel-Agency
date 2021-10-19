4c4facac319d88eaafaa7bcde069471eafe22fd2
Frasato <88912012+Frasato@users.noreply.github.com>
2021-10-19 19:35:29 -0300

    Add files via upload

---
+++ rio.js
@@ -0,0 +1,31 @@
+//variáveis
+var ima = window.document.getElementById('imgs');
+var ima2 = window.document.getElementById('imgs1');
+var ima3 = window.document.getElementById('imgs2');
+
+//img 1
+function bigimg(){
+    ima.style.maxWidth = '600px';
+}
+
+function smallimg(){
+    ima.style.maxWidth = '300px';
+}
+
+//img 2
+function bigimg2(){
+    ima2.style.maxWidth = '600px';
+}
+
+function smallimg2(){
+    ima2.style.maxWidth = '300px';
+}
+
+//img 3
+function bigimg3(){
+    ima3.style.maxWidth = '600px';
+}
+
+function smallimg3(){
+    ima3.style.maxWidth = '300px';
+}
---
+++ script.js
@@ -0,0 +1,5 @@
+var cont = window.document.getElementById('contato');
+
+function contato(){
+    cont.innerHTML = 'Obrigado por entrar em contato';
+}
